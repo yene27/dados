01e446c0b27861689ae273d13dedbfdd26527146
yene27 <yene27@yahoo.com>
2022-06-21 07:41:19 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,58 +1,63 @@
 let numero = 0
-input.onGesture(Gesture.Shake, function () {
-    numero = randint(1, 6)
-    if (numero == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-    } else {
-        if (numero == 2) {
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.spring), SoundExpressionPlayMode.UntilDone)
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) || input.isGesture(Gesture.Shake)) {
+        numero = randint(1, 6)
+        if (numero == 1) {
             basic.showLeds(`
                 . . . . .
-                . . . # .
                 . . . . .
-                . # . . .
+                . . # . .
+                . . . . .
                 . . . . .
                 `)
         } else {
-            if (numero == 3) {
+            if (numero == 2) {
                 basic.showLeds(`
                     . . . . .
                     . . . # .
-                    . . # . .
+                    . . . . .
                     . # . . .
                     . . . . .
                     `)
             } else {
-                if (numero == 4) {
+                if (numero == 3) {
                     basic.showLeds(`
                         . . . . .
-                        . # . # .
-                        . . . . .
-                        . # . # .
+                        . . . # .
+                        . . # . .
+                        . # . . .
                         . . . . .
                         `)
                 } else {
-                    if (numero == 5) {
+                    if (numero == 4) {
                         basic.showLeds(`
                             . . . . .
                             . # . # .
-                            . . # . .
+                            . . . . .
                             . # . # .
                             . . . . .
                             `)
                     } else {
-                        basic.showLeds(`
-                            . . . . .
-                            . # . # .
-                            . # . # .
-                            . # . # .
-                            . . . . .
-                            `)
+                        if (numero == 5) {
+                            basic.showLeds(`
+                                . . . . .
+                                . # . # .
+                                . . # . .
+                                . # . # .
+                                . . . . .
+                                `)
+                        } else {
+                            basic.showLeds(`
+                                . . . . .
+                                . # . # .
+                                . # . # .
+                                . # . # .
+                                . . . . .
+                                `)
+                        }
                     }
                 }
             }
